b1eed7e72493d391d2227b57e4ced543cb2d1a5e
Ye Eun Lee <117133205+JumboYogurt@users.noreply.github.com>
2023-09-12 17:51:35 +0900

    9/12

--- Assets/Scripts/Player_Scripts/CharacterEntity.cs
+++ Assets/Scripts/Player_Scripts/CharacterEntity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterEntity : IDamageable
+public class CharacterEntity :  MonoBehaviour, IDamageable
 {
     protected float hp = 10.0f;
     protected float damage = 10.0f;
--- Assets/Scripts/Player_Scripts/PlayerAttack.cs
+++ Assets/Scripts/Player_Scripts/PlayerAttack.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
+using System;
 
-public abstract class PlayerAttack : CharacterEntity
+public class PlayerAttack : CharacterEntity
 {
+    protected GameObject explosionFx = null;
+    private GameObject gameObject;
+
     // Start is called before the first frame update
-    void Start()
+    public override void OnTakeDamage(float _damage)
     {
-        
+        hp -= _damage;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+            //Instantiate(explosionFx, transform.position, transform.rotation);
+            //GameLogic.Inst.AddScore((int)enemyType);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Destroy(GameObject gameObject)
     {
-        
+        throw new NotImplementedException();
     }
-    public void Attack()
-    {
 
+    private void OnTriggerEnter(Collision coll)
+    {
+        //explosionSfx.Play();
+
+
+        if (coll.gameObject.tag == "Player")
+        {
+            //if (GameLogic.Inst.IsGameState(GameState.clear))
+                //return;
+            coll.gameObject.SendMessage("Damage", 3f);
+            OnTakeDamage(3f);
+            //this.Damage(3f);
+        }
     }
 }
